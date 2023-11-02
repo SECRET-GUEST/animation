15eb38428090ab4971117a592f638a4f605572b6
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-11-02 16:57:24 +0100

    Update cami_logo.py

--- Object generation/random/teardrop/CAMI/cami_logo.py
+++ Object generation/random/teardrop/CAMI/cami_logo.py
@@ -1,5 +1,73 @@
+#      |               |                                 |
+                
+#                  |                                                |            |                                   |                               |                           |                               |                           |                               |                                |                               |
+#           |                                  |                                     |
+                
+#                  |                     |                                                     |                                   |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#   |                          |                       |                    |
+#           |                                    |                                         |     |                    |                                    | |     |                    |                                    | |     |                    |                                    |      |     |                                                        |
+#      |                        |                                 |      |                                |                       |                    |                |                       |                    |                |                       |                    |                     |                       |                    |
+                
+#                      |                                                |      |                                   |                               |                         |                               |                         |                               |                              |                               |
+#           |                                   |                               |                   |                                |                       |                    |          |                                |                       |                    |          |                                |                       |                    |               |                                |                                        |
+                
+#                  |                     |
+#   |                                |                       |                    |            |                                |                       |                    |     |                                |                       |                    |     |                                |                       |                              |                                |
+#           |                               |                                         |                              |                       |                    |                           |                       |                    |                           |                       |                    |                                |                       |                    |
+                
+#   |         |                                   PRESENTATION                                                |                                |                    |   |                                |                    |   |                                |                    |        |                                |                    |
+#                                                                                                                               |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#                |                             /                 \                          |                                                                              |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+                
+#        |                                This script allows Blender to 
+                
+#                                   generate a logo of CAMI (Common Artificial               |                                           |               |                                           |               |                                           |                    |                                           |
+                
+#                           /                      |    v    |                    \
+#                 Modular Intelligence) based on gathering of several Artificial intelligences
+#                                   |                                |                                          |      |                                |                                          |      |                                |                                          |           |                                |
+#                                                                                                    |                                                                 |                      |                     |                       |                      |                     |                       |                      |                     |                            |
+#           
+#      |                  !      |                                   |     |                                |                       |                    |                |                       |                    |                |                       |                    |                    |                                           |
+#                                |                                   |     |                  
+#                   |            |                   Anyway          !                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                    |
+                
+#              |                      |                 have                                                 |                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                     |                                           |
+                
+
+#                 |                                  FUN         |                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                    |                                                                      |
+                
+#                                                         =)
+                
+#
+#                                |                      or                                       |                                                            |                    |                |                       |                    |                |                       |                    |                    |                                           |
+                
+
+#              |                              |       DIE ! ! !        |                       |                            |                |                       |                    |                |                       |                    |                    |                                           |#      |                        |                                         |                                |
+                
+#
+#                                                     !                                       |                                |                    |  |                                |                    |  |                                |                    |       |                                |                    |
+                
+#      |               |                                 !
+                
+#                  |                                                |            |                                   |                               |                           |                               |                           |                               |                                |                               |
+#           |                                  !                                     |
+                
+#                  |                     |                                                     |                                   |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#   |                          |                       |                    !
+#           |                                    |                                         |     |                    |                                    | |     |                    |                                    | |     |                    |                                    |      |     |                    |                                    |
+              
+#_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
+#| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
+#| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|
 import bpy
 import math
+
+#___  ____ _ _ _ ____ ____    ___  _    ____ _  _ ___
+#|__] |  | | | | |___ |__/    |__] |    |__| |\ |  |
+#|    |__| |_|_| |___ |  \    |    |___ |  | | \|  |
+                
+#OPENING | https://www.youtube.com/watch?v=qmk3Rri0jsQ&ab_channel=SECRETGUEST
 
 def create_teardrop(name, width, height, thickness):
     # Create a new curve object
@@ -36,11 +104,12 @@ def create_teardrop(name, width, height, thickness):
 
     return curve_obj
 
-
-# generate 3 teardrop
+# generate 3 teardrops
 teardrop1 = create_teardrop('Teardrop1', 3.5, 4, 0.1)
 teardrop2 = create_teardrop('Teardrop2', 3.5, 4, 0.1)
 teardrop3 = create_teardrop('Teardrop3', 3.5, 4, 0.1)
+
+
 
 # Move up each teardrops
 for teardrop in [teardrop1, teardrop2, teardrop3]:
@@ -73,3 +142,38 @@ teardrop2.select_set(False)
 teardrop3.select_set(True)
 bpy.context.view_layer.objects.active = teardrop3
 bpy.ops.transform.rotate(value=math.radians(240), orient_axis='Z', center_override=empty.location)
+
+
+
+# Place all in collections
+if "teardrops" not in bpy.data.collections:
+    teardrops = bpy.data.collections.new("teardrops")
+    bpy.context.scene.collection.children.link(teardrops)
+else:
+    teardrops = bpy.data.collections["teardrops"]
+
+# Unlink from default collection and link to teardrops collection
+default_collection = bpy.context.scene.collection
+for obj in [teardrop1, teardrop2, teardrop3]:
+    default_collection.objects.unlink(obj)
+    teardrops.objects.link(obj)
+
+# Convert each teardrop to mesh
+bpy.ops.object.select_all(action='DESELECT')
+for teardrop in [teardrop1, teardrop2, teardrop3]:
+    teardrop.select_set(True)
+    bpy.context.view_layer.objects.active = teardrop
+    bpy.ops.object.convert(target='MESH')
+    teardrop.select_set(False)
+
+# Delete the empty object
+bpy.ops.object.select_all(action='DESELECT')
+empty.select_set(True)
+bpy.ops.object.delete()
+
+
+# Remove constraints linking the 3 item to the center of the world
+for teardrop in [teardrop1, teardrop2, teardrop3]:
+    constraint_to_remove = [c for c in teardrop.constraints if c.type == 'TRACK_TO']
+    for constraint in constraint_to_remove:
+        teardrop.constraints.remove(constraint)
