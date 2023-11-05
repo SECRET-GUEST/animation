86ca485f45c851d627832b3fd920c6c9e0ef43c6
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-11-05 04:38:35 +0100

    Update integrator.py

--- EXTRACTORS/INTEGRATOR/integrator.py
+++ EXTRACTORS/INTEGRATOR/integrator.py
@@ -69,37 +69,39 @@ import os
                                    
 #OPENING | https://www.youtube.com/watch?v=qmk3Rri0jsQ&ab_channel=SECRETGUEST
 
-
 # Set the directory containing your scripts /path/to/your/script/directory"
 script_directory = ""
-execute_scripts = True  # Set to False if you only want to import scripts without executing them
+
+# Set to False if you don't want to include subdirectories
+include_subdirectories = True  
+
+# Set to False if you just want to import the scripts without executing them
+execute_scripts = True  
+
 
 #___  ____ _ _ _ ____ ____    ___  _    ____ _  _ ___
 #|__] |  | | | | |___ |__/    |__] |    |__| |\ |  |
 #|    |__| |_|_| |___ |  \    |    |___ |  | | \|  |
                 
 
-def execute_scripts_in_directory(directory, execute=True):
-    # Get a list of all files in the directory
-    files = os.listdir(directory)
-    
-    # Filter the list to only include .py files
-    script_files = [f for f in files if f.endswith('.py')]
-    
-    for script_file in script_files:
-        # Build the full path to the script file
-        script_path = os.path.join(directory, script_file)
+def execute_scripts_in_directory(directory, execute=True, include_subdirs=False):
+    for root, dirs, files in os.walk(directory):
+        # If not including subdirectories, empty the dirs list to prevent os.walk from traversing into subdirectories
+        if not include_subdirs:
+            del dirs[:]
+            
+        script_files = [f for f in files if f.endswith('.py')]
         
-        # Read the script file
-        with open(script_path, 'r') as file:
-            script_text = file.read()
-        
-        if execute:
-            # Execute the script
-            exec(script_text)
-        else:
-            # Import the script into Blender's Text Editor
-            text_block = bpy.data.texts.load(script_path, internal=True)
+        for script_file in script_files:
+            script_path = os.path.join(root, script_file)
+            
+            with open(script_path, 'r') as file:
+                script_text = file.read()
+            
+            if execute:
+                exec(script_text)
+            else:
+                text_block = bpy.data.texts.load(script_path, internal=True)
 
 # Call the function to execute or import all scripts in the specified directory
-execute_scripts_in_directory(script_directory, execute=execute_scripts)
+execute_scripts_in_directory(script_directory, execute=execute_scripts, include_subdirs=include_subdirectories)
