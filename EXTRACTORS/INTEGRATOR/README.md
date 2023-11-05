74c0e408943199d5375b3e4fc049215183197ddf
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-11-05 04:25:22 +0100

    Add files via upload

---
+++ EXTRACTORS/INTEGRATOR/README.md
@@ -0,0 +1,35 @@
+[![Blender](https://img.shields.io/badge/Blender-3.6-orange.svg)](https://www.blender.org/download/releases/3-6/)
+[![Python](https://img.shields.io/badge/Python-3.10.13-blue.svg)](https://www.python.org/downloads/release/python-31013/)
+```
+██╗███╗   ██╗████████╗███████╗ ██████╗ ██████╗  █████╗ ████████╗ ██████╗ ██████╗ 
+██║████╗  ██║╚══██╔══╝██╔════╝██╔════╝ ██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+██║██╔██╗ ██║   ██║   █████╗  ██║  ███╗██████╔╝███████║   ██║   ██║   ██║██████╔╝
+██║██║╚██╗██║   ██║   ██╔══╝  ██║   ██║██╔══██╗██╔══██║   ██║   ██║   ██║██╔══██╗
+██║██║ ╚████║   ██║   ███████╗╚██████╔╝██║  ██║██║  ██║   ██║   ╚██████╔╝██║  ██║
+╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+```
+
+# Executor and Importer 
+
+This repository contains a script `inegrator.py` that allows you to either execute or import multiple Python scripts into Blender in a batch manner. This is useful when you have a collection of scripts that you want to run or have available within Blender's Text Editor all at once.
+
+## Features
+
+- Batch execute or import Python scripts located in a specified directory.
+- Option to execute scripts immediately or just import them into Blender's Text Editor for manual execution later.
+
+## How to Use
+
+1. Clone or download this repository to your local machine.
+2. Open Blender.
+3. Go to the Text Editor view.
+4. Create a new text data-block and paste the contents of `inegrator.py` into it.
+5. Modify the `script_directory` variable at the top of the script to point to the directory containing the scripts you want to execute or import.
+6. Modify the `execute_scripts` variable to `True` if you want to execute the scripts, or `False` if you want to import them into the Text Editor without executing.
+7. Press the "Run Script" button to run the `inegrator.py` script.
+
+The scripts in the specified directory will now either be executed or imported into Blender's Text Editor based on your setting.
+
+---
+
+This README explains the updated functionality of choosing between executing the scripts or just importing them into Blender's Text Editor. It provides a step-by-step guide on how to use the `inegrator.py` script in Blender, and how to modify the parameters to fit the user's needs.
---
+++ EXTRACTORS/INTEGRATOR/integrator.py
@@ -0,0 +1,105 @@
+#      |               |                                 |
+                
+#                  |                                                |            |                                   |                               |                           |                               |                           |                               |                                |                               |
+#           |                                  |                                     |
+                
+#                  |                     |                                                     |                                   |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#   |                          |                       |                    |
+#           |                                    |                                         |     |                    |                                    | |     |                    |                                    | |     |                    |                                    |      |     |                                                        |
+#      |                        |                                 |      |                                |                       |                    |                |                       |                    |                |                       |                    |                     |                       |                    |
+                
+#                      |                                                |      |                                   |                               |                         |                               |                         |                               |                              |                               |
+#           |                                   |                               |                   |                                |                       |                    |          |                                |                       |                    |          |                                |                       |                    |               |                                |                                        |
+                
+#                  |                     |
+#   |                                |                       |                    |            |                                |                       |                    |     |                                |                       |                    |     |                                |                       |                              |                                |
+#           |                               |                                         |                              |                       |                    |                           |                       |                    |                           |                       |                    |                                |                       |                    |
+                
+#   |         |                                   PRESENTATION                                                |                                |                    |   |                                |                    |   |                                |                    |        |                                |                    |
+#                                                                                                                               |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#                |                             /                 \                          |                                                                              |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+                
+#        |                                This script allows Blender to 
+                
+#                             integrate easily all scripts from a folder in blender         |                                           |               |                                           |               |                                           |                    |                                           |
+                
+#                           /                      |    v    |                    \
+#                       https://github.com/SECRET-GUEST/animation/tree/blender/EXTRACTORS
+#                                   |                                |                                          |      |                                |                                          |      |                                |                                          |           |                                |
+#                                                                                                    |                                                                 |                      |                     |                       |                      |                     |                       |                      |                     |                            |
+#           
+#      |                  !      |                                   |     |                                |                       |                    |                |                       |                    |                |                       |                    |                    |                                           |
+#                                |                                   |     |                  
+#                   |            |                   Anyway          !                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                    |
+                
+#              |                      |                 have                                                 |                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                     |                                           |
+                
+
+#                 |                                  FUN         |                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                    |                                                                      |
+                
+#                                                         =)
+                
+#
+#                                |                      or                                       |                                                            |                    |                |                       |                    |                |                       |                    |                    |                                           |
+                
+
+#              |                              |       DIE ! ! !        |                       |                            |                |                       |                    |                |                       |                    |                    |                                           |#      |                        |                                         |                                |
+                
+#
+#                                                     !                                       |                                |                    |  |                                |                    |  |                                |                    |       |                                |                    |
+                
+#      |               |                                 !
+                
+#                  |                                                |            |                                   |                               |                           |                               |                           |                               |                                |                               |
+#           |                                  !                                     |
+                
+#                  |                     |                                                     |                                   |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#   |                          |                       |                    !
+#           |                                    |                                         |     |                    |                                    | |     |                    |                                    | |     |                    |                                    |      |     |                    |                                    |
+              
+#_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
+#| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
+#| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|
+import bpy
+import os
+
+#____ ____ ___ ___ _ _  _ ____ ____ 
+#[__  |___  |   |  | |\ | | __ [__  
+#___] |___  |   |  | | \| |__] ___] 
+                                   
+#OPENING | https://www.youtube.com/watch?v=qmk3Rri0jsQ&ab_channel=SECRETGUEST
+
+
+# Set the directory containing your scripts /path/to/your/script/directory"
+script_directory = ""
+execute_scripts = True  # Set to False if you only want to import scripts without executing them
+
+#___  ____ _ _ _ ____ ____    ___  _    ____ _  _ ___
+#|__] |  | | | | |___ |__/    |__] |    |__| |\ |  |
+#|    |__| |_|_| |___ |  \    |    |___ |  | | \|  |
+                
+
+def execute_scripts_in_directory(directory, execute=True):
+    # Get a list of all files in the directory
+    files = os.listdir(directory)
+    
+    # Filter the list to only include .py files
+    script_files = [f for f in files if f.endswith('.py')]
+    
+    for script_file in script_files:
+        # Build the full path to the script file
+        script_path = os.path.join(directory, script_file)
+        
+        # Read the script file
+        with open(script_path, 'r') as file:
+            script_text = file.read()
+        
+        if execute:
+            # Execute the script
+            exec(script_text)
+        else:
+            # Import the script into Blender's Text Editor
+            text_block = bpy.data.texts.load(script_path, internal=True)
+
+# Call the function to execute or import all scripts in the specified directory
+execute_scripts_in_directory(script_directory, execute=execute_scripts)
