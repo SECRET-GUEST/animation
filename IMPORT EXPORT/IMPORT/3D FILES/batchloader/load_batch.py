06ed463d80de27967a5934aed79bb8b1c450d8bc
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-11-23 11:33:55 +0100

    Update load_batch.py

--- IMPORT EXPORT/IMPORT/3D FILES/batchloader/load_batch.py
+++ IMPORT EXPORT/IMPORT/3D FILES/batchloader/load_batch.py
@@ -69,14 +69,61 @@ import time,bpy,os
 #OPENING | https://www.youtube.com/watch?v=qmk3Rri0jsQ&ab_channel=SECRETGUEST
 
 # Path to the folder containing 3D files
-folder_path = 'C:/path/to/the/folder' # Replace for your folder path
+folder_path = '/PUT/YOUR/PATH/HERE'   # Replace for your folder path
 include_subfolders = False            # Include subfolders in the folder selected ? 
 import_delay = 0                      # If big amount of files, you want to delay between each import to avoid blender crash
+
+# If both true, it will generate objects in one collection including collections for all objects
+include_in_a_single_collection = True
+include_in_object_collection = True
 
 #___  ____ _ _ _ ____ ____    ___  _    ____ _  _ ___
 #|__] |  | | | | |___ |__/    |__] |    |__| |\ |  |
 #|    |__| |_|_| |___ |  \    |    |___ |  | | \|  |
      
+     
+def create_or_get_collection(collection_name, parent_collection=None):
+    if collection_name in bpy.data.collections:
+        return bpy.data.collections[collection_name]
+    else:
+        new_collection = bpy.data.collections.new(collection_name)
+        if parent_collection:
+            parent_collection.children.link(new_collection)
+        else:
+            bpy.context.scene.collection.children.link(new_collection)
+        return new_collection
+
+def import_to_collection(filepath):
+    # Determine the collection names
+    main_collection_name = 'imported' if include_in_a_single_collection else None
+    object_collection_name = os.path.splitext(os.path.basename(filepath))[0] if include_in_object_collection else None
+
+    # Create or get the main collection
+    main_collection = create_or_get_collection(main_collection_name) if main_collection_name else None
+
+    # Create or get the object specific collection
+    object_collection = create_or_get_collection(object_collection_name, main_collection) if object_collection_name else main_collection
+
+    # Import the file
+    import_file(filepath)
+
+    # Add imported objects to the specified collection
+    if object_collection:
+        for obj in bpy.context.selected_objects:
+            # Ensure object isn't already linked to another collection
+            for coll in obj.users_collection:
+                coll.objects.unlink(obj)
+            object_collection.objects.link(obj)
+
+def import_file(filepath):
+    try:
+        extension = os.path.splitext(filepath)[1].lower()
+        # Import logic for different file formats (same as previously provided)
+    except Exception as e:
+        print(f"Error importing file {filepath}: {e}")
+    time.sleep(import_delay)  # Delay after each import
+
+
 
 def import_file(filepath):
     try:
@@ -119,9 +166,8 @@ def import_file(filepath):
 def process_folder(path):
     for entry in os.scandir(path):
         if entry.is_file():
-            import_file(entry.path)
+            import_to_collection(entry.path)
         elif entry.is_dir() and include_subfolders:
-            process_folder(entry.path) 
+            process_folder(entry.path)
 
 process_folder(folder_path)
-
