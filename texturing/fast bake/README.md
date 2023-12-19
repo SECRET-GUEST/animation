f4cd40a811cbc9ee2b23fe0de7d62a8597b2e816
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-12-19 23:43:30 +0100

    Add files via upload

---
+++ texturing/fast bake/README.md
@@ -0,0 +1,51 @@
+[![Blender](https://img.shields.io/badge/Blender-4.0-orange.svg)](https://www.blender.org/download/releases/4-0/)
+[![Python](https://img.shields.io/badge/Python-3.10.13-blue.svg)](https://www.python.org/downloads/release/python-31013/)
+```
+██████╗  █████╗ ██╗  ██╗███████╗    ███████╗ █████╗ ███████╗████████╗███████╗██████╗ 
+██╔══██╗██╔══██╗██║ ██╔╝██╔════╝    ██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+██████╔╝███████║█████╔╝ █████╗      █████╗  ███████║███████╗   ██║   █████╗  ██████╔╝
+██╔══██╗██╔══██║██╔═██╗ ██╔══╝      ██╔══╝  ██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗
+██████╔╝██║  ██║██║  ██╗███████╗    ██║     ██║  ██║███████║   ██║   ███████╗██║  ██║
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+```
+# baking texture faster
+
+This script is designed for Blender and allows users to bake the color of a material onto an object, preserving the appearance of the material even after applying a curve modifier. This is particularly useful for maintaining the appearance of striped or patterned materials on deformed objects.
+
+## Features
+
+- Bakes the color of a material onto a mesh object.
+- Preserves the material appearance post curve modifier application.
+- Customizable settings for texture resolution, file saving, and more.
+- Creates a new material with the baked texture and applies it to the object.
+- Option to save the baked texture as an image file.
+
+## Requirements
+
+- Blender (The script is tested with Blender 2.93, but should work with other versions).
+- A mesh object with a material applied to it.
+
+## Usage
+
+1. **Setting Parameters**: 
+   - Adjust the parameters at the beginning of the script according to your needs.
+   - Parameters include texture width, height, image name, and the option to save the image.
+
+2. **Running the Script**: 
+   - Open your Blender project and select the mesh object you want to bake.
+   - Open the Text Editor in Blender and paste the script.
+   - Run the script by pressing the 'Run Script' button.
+
+3. **Post-Bake**:
+   - After running the script, the selected object will have a new material with the baked texture.
+   - If `save_image` is set to `True`, the texture will be saved to the specified path.
+
+## Customization
+
+You can customize the script by modifying the following parameters:
+
+- `texture_width` and `texture_height`: Resolution of the baked texture.
+- `baked_img_name`: Name of the baked image.
+- `new_material_name`: Name of the new material created.
+- `save_image`: Set to `True` to save the image, or `False` to not save.
+- `image_save_path`: Filepath to save the baked image.
---
+++ texturing/fast bake/fast_texture_baking.py
@@ -0,0 +1,152 @@
+#      |               |                                 |
+                
+#                  |                                                |            |                                   |                               |                           |                               |                           |                               |                                |                               |
+#           |                                  |                                     |
+                
+#                  |                     |                                                     |                                   |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#   |                          |                       |                    |
+#           |                                    |                                         |     |                    |                                    | |     |                    |                                    | |     |                    |                                    |      |     |                                                        |
+#      |                        |                                 |      |                                |                       |                    |                |                       |                    |                |                       |                    |                     |                       |                    |
+                
+#                      |                                                |      |                                   |                               |                         |                               |                         |                               |                              |                               |
+#           |                                   |                               |                   |                                |                       |                    |          |                                |                       |                    |          |                                |                       |                    |               |                                |                                        |
+                
+#                  |                     |
+#   |                                |                       |                    |            |                                |                       |                    |     |                                |                       |                    |     |                                |                       |                              |                                |
+#           |                               |                                         |                              |                       |                    |                           |                       |                    |                           |                       |                    |                                |                       |                    |
+                
+#   |         |                                   PRESENTATION                                                |                                |                    |   |                                |                    |   |                                |                    |        |                                |                    |
+#                                                                                                                               |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#                |                             /                 \                          |                                                                              |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+                
+#        |                                This script allows Blender to 
+                
+#                              make a new material from previous one baked fast           |                                           |               |                                           |               |                                           |                    |                                           |
+                
+#                           /                      |    v    |                    \
+#                 
+#                                   |                                |                                          |      |                                |                                          |      |                                |                                          |           |                                |
+#                                                                                                    |                                                                 |                      |                     |                       |                      |                     |                       |                      |                     |                            |
+#           
+#      |                  !      |                                   |     |                                |                       |                    |                |                       |                    |                |                       |                    |                    |                                           |
+#                                |                                   |     |                  
+#                   |            |                   Anyway          !                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                    |
+                
+#              |                      |                 have                                                 |                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                     |                                           |
+                
+
+#                 |                                  FUN         |                        |                                         |                                |                       |                    |                |                       |                    |                |                       |                    |                    |                                                                      |
+                
+#                                                         =)
+                
+#
+#                                |                      or                                       |                                                            |                    |                |                       |                    |                |                       |                    |                    |                                           |
+                
+
+#              |                              |       DIE ! ! !        |                       |                            |                |                       |                    |                |                       |                    |                    |                                           |#      |                        |                                         |                                |
+                
+#
+#                                                     !                                       |                                |                    |  |                                |                    |  |                                |                    |       |                                |                    |
+                
+#      |               |                                 !
+                
+#                  |                                                |            |                                   |                               |                           |                               |                           |                               |                                |                               |
+#           |                                  !                                     |
+                
+#                  |                     |                                                     |                                   |                               |     |                                   |                               |     |                                   |                               |          |                                   |                               |
+#   |                          |                       |                    !
+#           |                                    |                                         |     |                    |                                    | |     |                    |                                    | |     |                    |                                    |      |     |                    |                                    |
+              
+#_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
+#| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
+#| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|
+import bpy
+
+#____ ____ ___ ___ _ _  _ ____ ____ 
+#[__  |___  |   |  | |\ | | __ [__  
+#___] |___  |   |  | | \| |__] ___] 
+                                   
+#OPENING | https://www.youtube.com/watch?v=qmk3Rri0jsQ&ab_channel=SECRETGUEST
+
+# texture size
+texture_width = 8192
+texture_height = 8192
+
+# names
+baked_img_name = "BakedTexture"
+new_material_name = "BakedMaterial"
+
+#save 
+save_image = True  # Set to False if you don't want to save the image
+image_save_path = "//" + baked_img_name + ".png"  # Change this to your desired path
+
+#___  ____ _ _ _ ____ ____    ___  _    ____ _  _ ___
+#|__] |  | | | | |___ |__/    |__] |    |__| |\ |  |
+#|    |__| |_|_| |___ |  \    |    |___ |  | | \|  |
+
+# Check if an object is selected
+if bpy.context.selected_objects:
+    selected_obj = bpy.context.selected_objects[0]
+
+    if selected_obj.type == 'MESH':
+        # Switch to edit mode and apply Smart UV Project
+        bpy.context.view_layer.objects.active = selected_obj
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.uv.smart_project()
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        # Create a new image for baking
+        baked_img = bpy.data.images.new(baked_img_name, width=texture_width, height=texture_height)
+
+        # Prepare the material for baking
+        mat = selected_obj.material_slots[0].material
+        mat.use_nodes = True
+        nodes = mat.node_tree.nodes
+
+        # Create a Texture node and configure for baking
+        tex_node = nodes.new('ShaderNodeTexImage')
+        tex_node.image = baked_img
+        tex_node.select = True
+        nodes.active = tex_node
+
+        # Set the render engine to Cycles for baking
+        bpy.context.scene.render.engine = 'CYCLES'
+        bpy.context.scene.cycles.bake_type = 'DIFFUSE'
+        bpy.context.scene.render.bake.use_pass_direct = False
+        bpy.context.scene.render.bake.use_pass_indirect = False
+
+        # Prepare the object for baking
+        for slot in selected_obj.material_slots:
+            slot.material.node_tree.nodes.active = tex_node
+
+        # Perform the bake
+        bpy.ops.object.bake(type='DIFFUSE')
+
+        # Create a new material with the baked texture
+        new_mat = bpy.data.materials.new(name=new_material_name)
+        new_mat.use_nodes = True
+        bsdf = new_mat.node_tree.nodes.get('Principled BSDF')
+        tex_image = new_mat.node_tree.nodes.new('ShaderNodeTexImage')
+        tex_image.image = baked_img
+        new_mat.node_tree.links.new(bsdf.inputs['Base Color'], tex_image.outputs['Color'])
+
+        # Assign the new baked material to the object
+        if selected_obj.data.materials:
+            # Replace the existing material
+            selected_obj.data.materials[0] = new_mat
+        else:
+            # Add the new material
+            selected_obj.data.materials.append(new_mat)
+
+        # Save the baked image if required
+        if save_image:
+            baked_img.filepath_raw = image_save_path
+            baked_img.file_format = 'PNG'
+            baked_img.save()
+
+        # Clean up the nodes created for baking
+        nodes.remove(tex_node)
+
+else:
+    print("No object selected.")
