cc54a968fe79f11feff890647a3a552abd6b38a8
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2024-01-26 06:06:09 +0100

    Delete useful tools/object data renamer directory

--- useful tools/object data renamer/mesh_object_data_renamer.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-# Rename the mesh object datas
-for obj in bpy.data.objects:
-    if obj.type == 'MESH' and obj.users_collection:
-        # Use first collection name then object name
-        collection_name = obj.users_collection[0].name
-        obj.data.name = collection_name + "_" + obj.name
